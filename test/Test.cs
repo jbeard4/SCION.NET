b516c8ee823476a9a5aea8b459ae2bd39a098383
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-06-14 12:13:00 -0400

    Converted tabs to spaces.

--- test/Test.cs
+++ test/Test.cs
@@ -11,16 +11,16 @@ namespace Test
         public static void Main(string[] args)
         {
             SCXML scxml = new SCXML("/home/jbeard/workspace/scion/scxml-test-framework/test/basic/basic1.scxml");
-			System.Console.WriteLine(scxml);
-        	Object initialConfiguration = scxml.start();
-			System.Console.WriteLine(initialConfiguration);
+            System.Console.WriteLine(scxml);
+            Object initialConfiguration = scxml.start();
+            System.Console.WriteLine(initialConfiguration);
 
-			Set nextConfiguration = scxml.gen("t",null);
-        	System.Console.WriteLine(nextConfiguration);
-			
-			Type t = initialConfiguration.GetType();
-			System.Console.WriteLine(t);
-			
-		}
+            Set nextConfiguration = scxml.gen("t",null);
+            System.Console.WriteLine(nextConfiguration);
+            
+            Type t = initialConfiguration.GetType();
+            System.Console.WriteLine(t);
+            
+        }
     }
 }
